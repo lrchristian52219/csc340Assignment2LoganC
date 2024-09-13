ed0e550f9c6440a06747a1edbbc8c5dad0cec581
Logan Christian <lrchristian@uncg.edu>
2024-09-12 23:06:37 -0400

    Good

---
+++ demo/src/main/java/com/csc340/demo/ApiController.java
@@ -0,0 +1,18 @@
+package com.csc340.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ApiController {
+    Map<>
+}
---
+++ demo/src/main/java/com/csc340/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.csc340.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+	}
+
+
+
+}
